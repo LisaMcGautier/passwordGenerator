67c35541bfb5e48a7879cb8776e52f6648bb8876
Lisa McDonald <nixxxxer@aol.com>
2020-07-14 10:22:30 -0400

    Streamline Console

--- script.js
+++ script.js
@@ -1,39 +1,31 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// HOW CAN I MAKE SURE THAT JS READS THE passwordLength variable AS A NUMBER AND NOT A STRING?
-
-
 var passwordLength = prompt("How many characters would you like your password to contain?");
-console.log(passwordLength);
+// console.log(passwordLength);
 
 if (passwordLength < 8) {
   alert("Minimum password length is 8 characters.");
   passwordLength = prompt("How many characters would you like your password to contain?");
-  console.log(passwordLength);
+  // console.log(passwordLength);
 }
 
 if (passwordLength > 128) {
   alert("Maximum password length is 128 characters.");
   passwordLength = prompt("How many characters would you like your password to contain?");
-  console.log(passwordLength);
+  // console.log(passwordLength);
 }
 
 if (passwordLength <= 7 || passwordLength >= 129) {
   alert("Minimum password length is 8 characters. Maximum password length is 128 characters.");
   passwordLength = prompt("How many characters would you like your password to contain?");
-  console.log(passwordLength);
+  // console.log(passwordLength);
 }
 
 if (passwordLength >= 8 || passwordLength <= 128) {
   alert("You have chosen a password length of " + passwordLength + " characters.");
+  console.log(passwordLength);
 }
-
-
-// if(passwordLength !== number) {
-//   alert("You must enter a number.")
-// }
-
 
 var passwordStart = "";
 
@@ -43,34 +35,28 @@ var specialCharacters = confirm("Click OK to include $peci@l characters.");
 
 if (specialCharacters == true) {
   var special = ["!", "@", "#", "$", "%", "&", "*", "?", "+", "="];
-  // console.log(special);
-
+  
   alphabet.push.apply(alphabet, special);
-  // console.log(alphabet)
-
+  
   var randomNumber = Math.random();
   var randomValue = randomNumber * special.length;
   var randomIndex = Math.floor(randomValue);
-  // console.log("Random special: " + special[randomIndex]);
-
+  
   var firstCharacter = special[randomIndex];
   passwordStart = passwordStart + firstCharacter;
 }
 
-var numericChararacters = confirm("Click OK to include num3r1c characters.")
+var numericChararacters = confirm("Click OK to include numeric characters.")
 
 if (numericChararacters == true) {
   var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9,];
-  // console.log(numbers);
-
+  
   alphabet.push.apply(alphabet, numbers);
-  // console.log(alphabet);
-
+  
   randomNumber = Math.random();
   randomValue = randomNumber * numbers.length;
   randomIndex = Math.floor(randomValue);
-  // console.log("Random number: " + numbers[randomIndex]);
-
+  
   var secondCharacter = numbers[randomIndex];
   passwordStart = passwordStart + secondCharacter;
 }
@@ -79,16 +65,13 @@ var lowercaseCharacters = confirm("Click OK to include lowercase characters.");
 
 if (lowercaseCharacters == true) {
   var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-  // console.log(lowercase);
-
+  
   alphabet.push.apply(alphabet, lowercase);
-  // console.log(alphabet);
-
+  
   randomNumber = Math.random();
   randomValue = randomNumber * lowercase.length;
   randomIndex = Math.floor(randomValue);
-  // console.log("Random lowercase: " + lowercase[randomIndex]);
-
+  
   var thirdCharacter = lowercase[randomIndex];
   passwordStart = passwordStart + thirdCharacter;
 }
@@ -97,21 +80,16 @@ var uppercaseCharacters = confirm("Click OK to include UPPERCASE characters.");
 
 if (uppercaseCharacters == true) {
   var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-  // console.log(uppercase);
-
+  
   alphabet.push.apply(alphabet, uppercase);
-  // console.log(alphabet);
-
+  
   randomNumber = Math.random();
   randomValue = randomNumber * uppercase.length;
   randomIndex = Math.floor(randomValue);
-  // console.log("Random uppercase: " + uppercase[randomIndex]);
-
+  
   var fourthCharacter = uppercase[randomIndex];
   passwordStart = passwordStart + fourthCharacter;
 }
-
-// console.log(alphabet);
 
 console.log(passwordStart);
 
@@ -129,6 +107,7 @@ for (var i = 0; i < passwordLength - passwordStart.length; i++) {
 }
 
 console.log(password);
+
 console.log(password.length);
 
 
