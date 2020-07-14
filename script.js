a5a861a10c6ea0c4773daf63a45332c3c219b325
Lisa McDonald <nixxxxer@aol.com>
2020-07-14 10:07:39 -0400

    Reduce Console Logs

--- script.js
+++ script.js
@@ -43,33 +43,33 @@ var specialCharacters = confirm("Click OK to include $peci@l characters.");
 
 if (specialCharacters == true) {
   var special = ["!", "@", "#", "$", "%", "&", "*", "?", "+", "="];
-  console.log(special);
+  // console.log(special);
 
   alphabet.push.apply(alphabet, special);
-  console.log(alphabet)
+  // console.log(alphabet)
 
   var randomNumber = Math.random();
   var randomValue = randomNumber * special.length;
   var randomIndex = Math.floor(randomValue);
-  console.log("Random special: " + special[randomIndex]);
+  // console.log("Random special: " + special[randomIndex]);
 
   var firstCharacter = special[randomIndex];
   passwordStart = passwordStart + firstCharacter;
 }
 
-var numericChararacters = confirm("Click OK to include numeric characters.")
+var numericChararacters = confirm("Click OK to include num3r1c characters.")
 
 if (numericChararacters == true) {
   var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9,];
-  console.log(numbers);
+  // console.log(numbers);
 
   alphabet.push.apply(alphabet, numbers);
-  console.log(alphabet);
+  // console.log(alphabet);
 
   randomNumber = Math.random();
   randomValue = randomNumber * numbers.length;
   randomIndex = Math.floor(randomValue);
-  console.log("Random number: " + numbers[randomIndex]);
+  // console.log("Random number: " + numbers[randomIndex]);
 
   var secondCharacter = numbers[randomIndex];
   passwordStart = passwordStart + secondCharacter;
@@ -79,39 +79,39 @@ var lowercaseCharacters = confirm("Click OK to include lowercase characters.");
 
 if (lowercaseCharacters == true) {
   var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-  console.log(lowercase);
+  // console.log(lowercase);
 
   alphabet.push.apply(alphabet, lowercase);
-  console.log(alphabet);
+  // console.log(alphabet);
 
   randomNumber = Math.random();
   randomValue = randomNumber * lowercase.length;
   randomIndex = Math.floor(randomValue);
-  console.log("Random lowercase: " + lowercase[randomIndex]);
+  // console.log("Random lowercase: " + lowercase[randomIndex]);
 
   var thirdCharacter = lowercase[randomIndex];
   passwordStart = passwordStart + thirdCharacter;
 }
 
-var uppercaseCharacters = confirm("Click OK to include lowercase characters.");
+var uppercaseCharacters = confirm("Click OK to include UPPERCASE characters.");
 
 if (uppercaseCharacters == true) {
   var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-  console.log(uppercase);
+  // console.log(uppercase);
 
   alphabet.push.apply(alphabet, uppercase);
-  console.log(alphabet);
+  // console.log(alphabet);
 
   randomNumber = Math.random();
   randomValue = randomNumber * uppercase.length;
   randomIndex = Math.floor(randomValue);
-  console.log("Random uppercase: " + uppercase[randomIndex]);
+  // console.log("Random uppercase: " + uppercase[randomIndex]);
 
   var fourthCharacter = uppercase[randomIndex];
   passwordStart = passwordStart + fourthCharacter;
 }
 
-console.log(alphabet);
+// console.log(alphabet);
 
 console.log(passwordStart);
 
